d18a6520b35c846c725bd9fc200a986f8c70130c
arthur <arthur.broudoux@gmail.com>
2024-06-26 10:59:04 +0200

    feat: create db, AddSessions method Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "github.com/abroudoux/reiki-api/internal/router"
+import (
+	"log"
+
+	"github.com/abroudoux/reiki-api/internal/database"
+	"github.com/abroudoux/reiki-api/internal/router"
+)
 
 func main() {
+	err := database.CreateTableSessions()
+	if err != nil {
+		log.Fatalf("Failed to create sessions table: %v", err)
+	}
+
 	router.InitRouter()
 }
